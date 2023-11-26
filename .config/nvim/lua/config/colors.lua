5481198964eb72e17be82793da4b423c58672bec
bitterhalt <95308907+bitterhalt@users.noreply.github.com>
2023-11-26 16:51:03 +0200

    Some fixes

--- .config/nvim/lua/config/colors.lua
+++ .config/nvim/lua/config/colors.lua
@@ -23,7 +23,13 @@
 --vim.g.gruvbox_baby_background_color = "dark"
 --vim.g.gruvbox_baby_telescope_theme = 1
 --local _ = pcall(vim.cmd, "colorscheme gruvbox-baby")
-
+-- Alternatively, you can provide a table specifying your colors to the setup function.
+require('base16-colorscheme').setup({
+    base00 = '#16161D', base01 = '#2c313c', base02 = '#3e4451', base03 = '#6c7891',
+    base04 = '#565c64', base05 = '#abb2bf', base06 = '#9a9bb3', base07 = '#c5c8e6',
+    base08 = '#e06c75', base09 = '#d19a66', base0A = '#e5c07b', base0B = '#98c379',
+    base0C = '#56b6c2', base0D = '#0184bc', base0E = '#c678dd', base0F = '#a06949',
+})
 
 
 
