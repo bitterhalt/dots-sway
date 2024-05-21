5e679533c6d72cee7b03b77cb240b4ccfddd8832
bitterhalt <95308907+bitterhalt@users.noreply.github.com>
2024-05-21 23:41:33 +0300

    remove some modeles

--- .config/waybar/custom/todo.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-COUNT=$(wc -l < ~/Documents/personal/notes/todo.md)
-TODOS=$(cat ~/Documents/personal/notes/todo.md | head -c -1 - | sed -z 's/\n/\\n/g')
-printf '{"text": "%s", "tooltip": "%s"}\n' "$COUNT" "$TODOS"
