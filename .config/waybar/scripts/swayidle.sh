40ace92bd0d1e24fc9b473327d9dbf208ac24f13
bitterhalt <95308907+bitterhalt@users.noreply.github.com>
2024-05-24 23:35:23 +0300

    fix status

--- .config/waybar/scripts/swayidle.sh
+++ .config/waybar/scripts/swayidle.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 if pgrep -x "swayidle" > /dev/null; then
-    echo "{\"text\": \"󰒳\", \"tooltip\": \"<b>Swayidle:</b> active\"}"
-else
     echo ""
+else
+    echo "{\"text\": \"󰒳\", \"tooltip\": \"<b>Swayidle:</b> disabled\"}"
 fi
