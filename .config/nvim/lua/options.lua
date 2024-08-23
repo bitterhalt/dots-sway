acccf80d947a3d7cd54ce4afea6e2b3efc5b1bc7
bitterhalt <bazz@void.com>
2024-08-23 12:38:57 +0300

    use space as leader

--- .config/nvim/lua/options.lua
+++ .config/nvim/lua/options.lua
@@ -1,49 +1,37 @@
--- Interface
+-- Opts
+
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
-vim.opt.colorcolumn = '80'
-vim.opt.cursorline = true
-vim.opt.laststatus = 2
-vim.opt.number = true
-vim.opt.relativenumber = false
-vim.opt.syntax = 'on'
 vim.opt.termguicolors = true
-
--- Behavior
-vim.opt.clipboard = 'unnamedplus'
-vim.opt.confirm = true
 vim.opt.scrolloff = 4
-vim.opt.splitright = true
-vim.opt.splitbelow = true
-
--- Indentation
+vim.opt.number = true
+vim.opt.numberwidth = 2
+vim.opt.signcolumn = "yes"
+vim.opt.cursorline = true
+vim.opt.expandtab = true
+vim.opt.smarttab = true
+vim.opt.cindent = true
+vim.opt.autoindent = true
+vim.opt.wrap = true
+vim.opt.textwidth = 300
+vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.softtabstop = -1
-vim.opt.expandtab = true
-
--- Line wrapping
-vim.opt.breakindent = true
-vim.opt.linebreak = true
-
--- Searching
+vim.opt.list = true
+vim.opt.clipboard = "unnamedplus"
 vim.opt.ignorecase = true
 vim.opt.smartcase = true
-
--- Undo and backup options
 vim.opt.backup = false
 vim.opt.writebackup = false
 vim.opt.swapfile = false
+vim.opt.history = 50
+vim.opt.splitright = true
+vim.opt.splitbelow = true
+vim.opt.mouse = "a"
 
 -- Remove trailing whitespace on save
 vim.api.nvim_create_autocmd('BufWritePre', {
     command = '%s/\\s\\+$//e',
-})
-
--- Show diagnostics on dialog on cursor hover
-vim.api.nvim_create_autocmd('CursorHold', {
-    callback = function()
-        vim.diagnostic.open_float({ focusable = false })
-    end,
 })
 
 -- Highlight yanked text
