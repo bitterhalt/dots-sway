4441b4484e2f7a138643a4b9b717372f30ce8a9e
bitterhalt <bazz@void.com>
2024-08-26 09:35:23 +0300

    bordered cmp window

--- .config/nvim/lua/plugins/cmp.lua
+++ .config/nvim/lua/plugins/cmp.lua
@@ -24,6 +24,14 @@ return {
                     luasnip.lsp_expand(args.body)
                 end,
             },
+
+            -- Bordered window
+            window = {
+                completion = cmp.config.window.bordered(),
+                documentation = cmp.config.window.bordered(),
+            },
+
+
             mapping = cmp.mapping.preset.insert({
                 ["<C-d>"] = cmp.mapping.scroll_docs(-4),
                 ["<C-f>"] = cmp.mapping.scroll_docs(4),
