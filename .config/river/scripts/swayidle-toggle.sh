b8c4f3d60722edab2e621d518337f8862fcd2cec
bitterhalt <bazz@void.com>
2024-09-01 22:11:46 +0300

    fix swayidle scripts

--- .config/river/scripts/swayidle-toggle.sh
+++ .config/river/scripts/swayidle-toggle.sh
@@ -6,11 +6,8 @@ if  pgrep swayidle >/dev/null; then
 else
     notify-send -a center_notify "Swayidle" "Enabled" -t 1500 -h string:x-canonical-private-synchronous:volume
     swayidle -w \
-    timeout 1800 "swaylock -f" \
+    timeout 1800 "swaylock" \
     timeout 3600 "wlopm --off '*'" resume "wlopm --on '*'" \
-    before-sleep "swaylock -f" &
+    before-sleep "swaylock" &
 fi
 
-         timeout 1800 "swaylock -f" \
-         timeout 3600 "wlopm --off '*'" resume "wlopm --on '*'" \
-         before-sleep "swaylock -f" &
