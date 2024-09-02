9be356cd1fe40cc4e36ff0a5022ef82026cd3d92
bitterhalt <bazz@void.com>
2024-09-02 23:33:50 +0300

    Back good ol oxo

--- .config/nvim/lua/plugins/ui.lua
+++ .config/nvim/lua/plugins/ui.lua
@@ -1,48 +1,50 @@
 return {
     --Base - 16 - nvim
+    --  {
+    --      "RRethy/base16-nvim",
+    --      config = function()
+    --          require('base16-colorscheme').with_config({
+    --              telescope = false,
+    --              telescope_borders = false,
+    --              indentblankline = false,
+    --              notify = true,
+    --              ts_rainbow = false,
+    --              cmp = false,
+    --              illuminate = false,
+    --              lsp_semantic = false,
+    --              mini_completion = false,
+    --              dapui = false,
+    --          })
+    --      end,
+    --      init = function()
+    --          require("base16-colorscheme").setup({
+    --              base00 = '#161616',
+    --              base01 = '#262626',
+    --              base02 = '#393939',
+    --              base03 = '#525252',
+    --              base04 = '#b4b7b4',
+    --              base05 = '#c5c8c6',
+    --              base06 = '#e0e0e0',
+    --              base07 = '#ffffff',
+    --              base08 = '#AC4242',
+    --              base09 = '#F4BF75',
+    --              base0A = '#F4BF75',
+    --              base0B = '#90A959',
+    --              base0C = '#75B5AA',
+    --              base0D = '#6A9FB5',
+    --              base0E = '#AA759F',
+    --              base0F = '#C28CB8'
+    --          })
+    --      end,
+    --  },
+    --
+    -- Oxocarbon
     {
-        "RRethy/base16-nvim",
+        "nyoom-engineering/oxocarbon.nvim",
         config = function()
-            require('base16-colorscheme').with_config({
-                telescope = false,
-                telescope_borders = false,
-                indentblankline = false,
-                notify = true,
-                ts_rainbow = false,
-                cmp = false,
-                illuminate = false,
-                lsp_semantic = false,
-                mini_completion = false,
-                dapui = false,
-            })
-        end,
-        init = function()
-            require("base16-colorscheme").setup({
-                base00 = '#161616',
-                base01 = '#262626',
-                base02 = '#393939',
-                base03 = '#525252',
-                base04 = '#b4b7b4',
-                base05 = '#c5c8c6',
-                base06 = '#e0e0e0',
-                base07 = '#ffffff',
-                base08 = '#AC4242',
-                base09 = '#F4BF75',
-                base0A = '#F4BF75',
-                base0B = '#90A959',
-                base0C = '#75B5AA',
-                base0D = '#6A9FB5',
-                base0E = '#AA759F',
-                base0F = '#C28CB8'
-            })
+            require("oxocarbon")
         end,
     },
-    --   {
-    --       "nyoom-engineering/oxocarbon.nvim",
-    --       config = function()
-    --           require("oxocarbon")
-    --       end,
-    --   },
     -- Telescope
     {
         "nvim-telescope/telescope.nvim",
