42add26d6b7de9932615f7c21b21d86dec197a2a
bitterhalt <bazz@void.com>
2024-09-04 03:37:21 +0300

    mix the deck

--- .config/river/scripts/locker.sh
+++ .config/river/scripts/locker.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # Times the screen off and puts it to background
 swayidle \
-    timeout  120 "wlopm --toggle '*'" \
-    resume "wlopm --toggle '*'"  &
+    timeout 120 "wlopm --off '*'" \
+    resume "wlopm --on '*'" &
 # Locks the screen immediately
 swaylock
 # Kills last background task so idle timer doesn't keep running
--- .config/river/scripts/scratchpads.sh
+++ .config/river/scripts/scratchpads.sh
@@ -1,9 +1,8 @@
-#!/bin/bash
-#
+#!/usr/bin/env bash
+
 # Riverctl script to open scratchpads, and open programs if Scratchpads are
 # empty. Usage "./scratchpads.sh <term|lf|nvim>"
 #
-
 # Check if an argument is provided
 if [ -z "$1" ]; then
     echo "Error: No argument provided."
@@ -14,35 +13,35 @@ term="$term --app-id special-term"
 lf="$term --app-id special-lf -e lf"
 nvim="$term --app-id special-nvim -e nvim"
 
-
 terminal_tag=$((1 << 11))
 secrets_tag=$((1 << 12))
 chat_tag=$((1 << 13))
 case "$1" in
-    "term")
-        search="special-term"
-        tag="$terminal_tag"
-        cmd="$term"
-        ;;
-    "lf")
-        search="special-lf"
-        tag="$terminal_tag"
-        cmd="$lf"
-        ;;
-    "nvim")
-        search="special-nvim"
-        tag="$terminal_tag"
-        cmd="$nvim"
-        ;;
-    "keepassxc")
-        search="keepassxc"
-        tag="$secrets_tag"
-        cmd="keepassxc"
-        ;;
-    "discord")
-        search="discord"
-        tag="$chat_tag"
-        cmd="flatpak run com.discordapp.Discord"
+"term")
+    search="special-term"
+    tag="$terminal_tag"
+    cmd="$term"
+    ;;
+"lf")
+    search="special-lf"
+    tag="$terminal_tag"
+    cmd="$lf"
+    ;;
+"nvim")
+    search="special-nvim"
+    tag="$terminal_tag"
+    cmd="$nvim"
+    ;;
+"keepassxc")
+    search="keepassxc"
+    tag="$secrets_tag"
+    cmd="keepassxc"
+    ;;
+"discord")
+    search="discord"
+    tag="$chat_tag"
+    cmd="flatpak run com.discordapp.Discord"
+    ;;
 esac
 
 if lswt | grep -q "$search"; then
